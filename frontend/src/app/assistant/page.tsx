9dd997a4416709c468ed7be80cf5b0dc304542de
Soham <realsoham774@gmail.com>
2024-03-30 19:35:50 +0530

    buttons

--- frontend/src/app/assistant/page.tsx
+++ frontend/src/app/assistant/page.tsx
@@ -60,6 +60,7 @@ export default function page() {
 
   const [chatbotResponse, setChatbotResponse] = useState<string | null>(null);
   const [animatePrompt, setAnimatePrompt] = useState(false); // State to control animation
+  
   const handleTriggerClick = () => {
     setAnimatePrompt(true); // Toggle animation state
     setTimeout(() => setAnimatePrompt(false), 500); // Reset animation state after 500ms
@@ -170,7 +171,8 @@ export default function page() {
                       <CommandGroup heading="Suggestions">
                         <CommandItem>
                           <Calendar className="mr-2 h-4 w-4" />
-                          <span>"Remind me to submit the project at 5 PM tomorrow.</span>
+                          <span>Schedule an email reminder for the
+                            project deadline on Friday at 10 AM</span>
                         </CommandItem>
                         <CommandItem>
                           <Target className="mr-2 h-4 w-4" />
@@ -186,24 +188,22 @@ export default function page() {
                     <CommandInput placeholder="Input Prompt." />
                   </Command>
 
-
-                  <Button type="submit" size="icon" variant="outline" disabled  >
-                    <svg
+                  <button className="group h-8 select-none rounded-lg bg-gradient-to-b from-zinc-800 via-zinc-700 to-zinc-600 px-3 text-sm leading-8 text-zinc-50 shadow-[0_-1px_0_1px_rgba(0,0,0,0.8)_inset,0_0_0_1px_rgb(9_9_11)_inset,0_0.5px_0_1.5px_#71717a_inset] hover:bg-gradient-to-b hover:from-zinc-900 hover:via-zinc-900 hover:to-zinc-700 active:shadow-[0_3px_0_0_rgba(0,0,0)_inset]"><span className="block group-active:[transform:translate3d(0,1px,0)]">
+                  <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 256 256"
-                      fill=""
-                      className='size-4'
+                      fill="white "
+                      className='size-4 text-white'
 
                     >
                       <path d="M200 32v144a8 8 0 0 1-8 8H67.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L67.31 168H184V32a8 8 0 0 1 16 0Z" />
                     </svg>
-                    <span className="sr-only">Send message</span>
-                  </Button>
-                  <Button type="submit" size="icon" variant="outline" disabled >
-                    <Mic size={18} />
-
-                    <span className="sr-only">Send message</span>
-                  </Button>
+                    </span></button>
+                    <button className="group h-8 select-none rounded-lg bg-gradient-to-b from-zinc-800 via-zinc-700 to-zinc-600 px-3 text-sm leading-8 text-zinc-50 shadow-[0_-1px_0_1px_rgba(0,0,0,0.8)_inset,0_0_0_1px_rgb(9_9_11)_inset,0_0.5px_0_1.5px_#71717a_inset] hover:bg-gradient-to-b hover:from-zinc-900 hover:via-zinc-900 hover:to-zinc-700 active:shadow-[0_3px_0_0_rgba(0,0,0)_inset]"><span className="block group-active:[transform:translate3d(0,1px,0)]">
+                    <Mic size={15} />
+                    </span></button>
+                  
+                  
                 </div>
 
 
