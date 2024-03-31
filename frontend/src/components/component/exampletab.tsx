327c0d55a52c0e0e0b84e798357f44ca03e86860
Saniyaa B Shetty <saniyaab@sheety.com>
2024-03-31 07:13:19 +0530

    tabs added

--- frontend/src/components/component/exampletab.tsx
+++ frontend/src/components/component/exampletab.tsx
@@ -8,17 +8,17 @@ export default function TabsDemo() {
       title: "Speech recognition",
       value: "Speech recognition",
       content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-green-700 bg-gradient-to-br from-slate-50 to-slate-50 border-green-700 border-solid border-2">
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-green-700 bg-gradient-to-br from-green-300 to-green-500 ">
           <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
         <div className='flex flex-col gap-2'>
          
+          <DummyContent imageSrc="/speech-recognition.png" />ß
           <h1 className='text-md md:text-xl'>Speech Recognition and Personalization</h1>
         </div>
         <p className='max-w-md text-base md:text-base text-green-400'>Highly accurate speech recognition</p>
         <p className='max-w-md text-base md:text-base text-green-400'>Voice profile recognition system that can identify individual users based on their unique voice.</p>
         <p className='max-w-md text-base md:text-base text-green-400'>Integrated terminal to accept terminal-style commands in natural language.</p>
       </div>
-          <DummyContent imageSrc="/landing/images/1.png" />
         </div>
       ),
     },
@@ -26,7 +26,7 @@ export default function TabsDemo() {
       title: "Information Retrieval and Task Execution",
       value: "Information Retrieval and Task Execution",
       content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-pink-700 bg-gradient-to-br from-slate-50 to-slate-50 border-pink-700 border-solid border-2">
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-pink-700 bg-gradient-to-br from-pink-200 to-pink-500">
           <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
         <div className='flex flex-col gap-2'>
          
@@ -36,7 +36,7 @@ export default function TabsDemo() {
         <p className='max-w-md text-base md:text-base text-pink-400'>Ability to execute a variety of predefined tasks based on voice input.</p>
         <p className='max-w-md text-base md:text-base text-pink-400'>Intelligent task matching.</p>
       </div>
-          <DummyContent imageSrc="/landing/images/2.png" />
+          <DummyContent imageSrc="/information-retrieval.png" />
         </div>
       ),
     },
@@ -44,7 +44,7 @@ export default function TabsDemo() {
         title: "Structured flow",
         value: "Structured flow",
         content: (
-          <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-purple-600 bg-gradient-to-br from-slate-50 to-slate-50 border-purple-600 border-solid border-2">
+          <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-purple-600 bg-gradient-to-br from-purple-300 to-purpe-700">
              <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
         <div className='flex flex-col gap-2'>
          
@@ -55,7 +55,7 @@ export default function TabsDemo() {
 </p>
         <p className='max-w-md text-base md:text-base text-purple-400'>Query time graphs</p>
       </div>
-            <DummyContent imageSrc="/landing/images/2.png" />
+            <DummyContent imageSrc="/user-friendly.png" />
           </div>
         ),
       },
@@ -63,7 +63,7 @@ export default function TabsDemo() {
         title: "Realistic voice",
         value: "Realistic voice",
         content: (
-          <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-yellow-500 bg-gradient-to-br from-slate-50 to-slate-50 border-yellow-500 border-solid border-2 flex flex-col md:flex-row items-center">
+          <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-yellow-500 bg-gradient-to-br from-yellow-300 to-slate-50">
    <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
 
         <div className='flex flex-col gap-2'>
@@ -74,7 +74,7 @@ export default function TabsDemo() {
         <p className='max-w-md text-lg md:text-base text-yellow-300'>Ability to maintain context across conversations to provide personalized responses.</p>
         <p className='max-w-md text-lg md:text-base text-yellow-300'>Learn and improve performance over time based on user feedback and usage patterns.</p>
       </div>
-            <DummyContent imageSrc="/landing/images/3.png" />
+            <DummyContent imageSrc="/speech-recognition.png" />
           </div>
         ),
       },
@@ -82,8 +82,7 @@ export default function TabsDemo() {
         title: "Security",
         value: "chat",
         content: (
-        <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
-          <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-blue-700 bg-gradient-to-br from-slate-50 to-slate-50 border-blue-700 border-solid border-2">
+          <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-blue-700 bg-gradient-to-br from-blue-300 to-blue-800">
             <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
         <div className='flex flex-col gap-2'>
          
@@ -95,9 +94,8 @@ export default function TabsDemo() {
       </div>
 
       <div className='w-full h-2/4 md:h-full md:w-3/5 flex items-center justify-center relative -z-10 ml-30'>
-      <DummyContent imageSrc="/landing/images/3.png" />
+      <DummyContent imageSrc="/security.png" />
       </div>
-          </div>
           </div>
         ),
       },
@@ -107,7 +105,7 @@ export default function TabsDemo() {
 
   return (
     <>
-    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#e0c9ff,transparent)]"></div></div>
+    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center"></div><div className="inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#e0c9ff,transparent)]"></div></div>
     <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
         
       <Tabs tabs={tabs} />
